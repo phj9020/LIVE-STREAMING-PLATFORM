55e9adbe10f4c1f6c3a3fbea9ee8f4bd9f401d21
박한진 <phj9020@hotmail.com>
2024-01-02 22:09:46 +0900

    auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,6 @@
 const AuthLayOut = ({children} : { children: React.ReactNode}) => {
   return (
-    <div className="flex flex-col gap-y-4">
-      <nav className="bg-red-500 w-full">
-        auth nav
-      </nav>
+    <div className="flex h-full items-center justify-center">
       {children}
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from '@/components/ui/button'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (  
-    <p>
-      only authenticated users can see this
-    </p>
+    <div className="flex flex-col gap-y-4">
+      <h1>dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
