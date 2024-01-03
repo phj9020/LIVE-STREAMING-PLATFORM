9be8e2f959c67a9a920129e3d72a46df2bfdfa40
박한진 <phj9020@hotmail.com>
2024-01-03 21:01:08 +0900

    Logo component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,9 @@
+import { Logo } from "./_components/logo";
+
 const AuthLayOut = ({children} : { children: React.ReactNode}) => {
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="flex flex-col h-full items-center justify-center space-y-6">
+      <Logo />
       {children}
     </div>
   )
