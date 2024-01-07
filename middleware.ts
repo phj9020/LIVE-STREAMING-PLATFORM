057050847f4c41d3ec40cf0b16812fdf218652da
박한진 <phj9020@hotmail.com>
2024-01-07 18:35:22 +0900

    clerk webhook applied with using ngrok(dev domain)

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+export default authMiddleware({
+  //app 내 api 경로 퍼블릭 경로 허용
+  publicRoutes: ["/api/webhooks(.*)"],
+});
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
